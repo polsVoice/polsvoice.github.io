35b9570725300a1b990087a878f7e143d7bb3f9c
Brent Bartlett <brent.bartlett1@gmail.com>
2023-03-13 08:49:25 -0700

    add fiveserver config

---
+++ fiveserver.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  open: "index.html", // open index.html when you click "Go Live"
+  // rmv this attribute if you want to open the current file instead
+  browser: "brave"
+  // open in a different browser than system default
+};
